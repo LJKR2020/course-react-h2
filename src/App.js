804f12a0615d124de7060a230f6c539ca5b389e5
LJKR2020 <loredana.rietveld@gmail.com>
2021-11-23 17:21:54 +0100

    3.1 tot 3.3 finished

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './App.css';
-import Product from "./Product";
+import Product from './Product';
 import citroenen from './assets/citroenen.jpeg';
 import limoenen from './assets/limoenen.png';
 import ijsblokjes from './assets/ijsblokjes.jpg';
-import { ReactComponent as Shoppingcart } from "./assets/winkelmandje.svg";
+import { ReactComponent as Shoppingcart } from './assets/winkelmandje.svg';
 
 function App() {
+    const [messageValue, setMessageValue] = React.useState('')
     return (
         <>
             <nav>
                 <ul>
-                    <li><a href="/">Shop</a></li>
-                    <li><a href="/">Ons verhaal</a></li>
-                    <li><a href="/">Blog</a></li>
+                    <li><a href='/'>Shop</a></li>
+                    <li><a href='/'>Ons verhaal</a></li>
+                    <li><a href='/'>Blog</a></li>
                 </ul>
-                <Shoppingcart className="shopping-cart-icon"/>
+                <Shoppingcart className='shopping-cart-icon'/>
             </nav>
             <header>
                 <h1>Fruit perfection</h1>
                 <button
-                    type="button"
-                    onClick={() => console.log("Jij wilt shoppen!")}
+                    type='button'
+                    onClick={() => console.log('Jij wilt shoppen!')}
                 >
                     Shop nu
                 </button>
@@ -29,20 +30,39 @@ function App() {
             <main>
                 <Product
                     image={citroenen}
-                    title="citroenen"
-                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                    title='citroenen'
+                    description='Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.'
                 />
                 <Product
                     image={limoenen}
-                    title="Limoenen"
-                    description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+                    title='Limoenen'
+                    description='Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.'
                 />
                 <Product
                     image={ijsblokjes}
-                    title="Ijsblokjes"
-                    description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+                    title='Ijsblokjes'
+                    description='Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.'
                 />
             </main>
+            <footer>
+                <div className='form-container'>
+                    <h2>Contactformulier</h2>
+
+                    <form>
+                        <input
+                            type='text'
+                            placeholder='Typ hier uw bericht'
+                            name='message'
+                            value={messageValue}
+                            onChange={(e) => setMessageValue(e.target.value)}
+                        />
+                    </form>
+
+                    <button type='submit'>
+                        Verstuur
+                    </button>
+                </div>
+            </footer>
         </>
     );
 }
